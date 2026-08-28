bb1915f9c0a57d21487fdfcf2da3f6ee6624fc2f
28mtohch <homebbpirapxt@gmail.com>
2026-08-28 19:59:32 +0700

    Add files via upload

---
+++ src/supabaseClient.js
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://vdkyemtnzjsqwxozthoz.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZka3llbXRuempzcXd4b3p0aG96Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODc4NTExMjAsImV4cCI6MjEwMzQyNzEyMH0.9qY4GBd0MCGqlsfhNjJ9L4AteiC-5PyGCo2JeEjuz7A'
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
