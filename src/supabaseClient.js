7cf8b1234ba739b28713c305620906d9151ae324
28mtohch <homebbpirapxt@gmail.com>
2026-09-07 18:11:42 +0700

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 // ใส่ URL และ Key ตรงๆ แบบนี้เลย
-const supabaseUrl = 'https://vdkyemtnzjsqwxozthoz.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZka3llbXRuempzcXd4b3p0aG96Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODc4NTExMjAsImV4cCI6MjEwMzQyNzEyMH0.9qY4GBd0MCGqlsfhNjJ9L4AteiC-5PyGCo2JeEjuz7A'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
